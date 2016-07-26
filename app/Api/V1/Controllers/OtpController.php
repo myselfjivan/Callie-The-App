aaa7ca8380ebbac82d01e0102373713b0b56cec5
myselfjivan <jivangahdage@gmail.com>
2016-07-26 19:29:21 +0530

    insert user to users table only if verified

--- app/Api/V1/Controllers/OtpController.php
+++ app/Api/V1/Controllers/OtpController.php
@@ -4,39 +4,48 @@ namespace App\Api\V1\Controllers;
 
 use App\Otp;
 use App\User;
+use DB;
 use App\Http\Requests;
 use Dingo\Api\Routing\Helpers;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 
 class OtpController extends Controller {
-
+    
     use Helpers;
     //
     public function p_register(Request $request) {
-
+        
         $pregister = new Otp;
-
+        
         $pregister->mobile = $request->get('mobile');
         $pregister->otp = str_random(8);
-
+        
         if ($pregister->save())
-            return $this->response->created();
+            return $pregister;
+        //return $this->response->created();
         else
             return $this->response->error('could_not_register_number', 500);
     }
-
+    
     public function p_verify(Request $request) {
-
+        
         $enterdOtp = $request->get('otp');
         $enterdMobile = $request->get('mobile');
-
+        
         $otps = \App\Otp::where('mobile', $enterdMobile)->get();
         foreach ($otps as $otp) {
             if ($enterdOtp == $otp->otp) {
                 $otp->v_status = 1;
-                if ($otp->save())
+                if ($otp->save()){
+                    DB::table('users')->insert(
+                    ['mobile' => $otp->mobile,
+                    'v_status' => 1,
+                    'apikey' => str_random(120) ]
+                    );
                     return $otp;
+                }
+                
                 else
                     echo $this->response->error('error_occured', 500);
             } else {
@@ -44,5 +53,5 @@ class OtpController extends Controller {
             }
         }
     }
-
+    
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email')->unique()->nullable();
             $table->string('mobile')->unique();
             $table->string('v_status');
